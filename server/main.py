c427454453bfc012a780aa084a74ca2ea3521d67
Chrahuldeveloper <chrahulofficial@gmail.com>
2025-12-22 16:32:46 +0530

    dn

--- server/main.py
+++ server/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 app  = FastAPI()
 
+
+@app.get("/detect")
+def predictMonument():
+    return { "message" : ""}
+
 @app.get("/")
 def root():
     return {"message": "FastAPI is running 🚀"}
