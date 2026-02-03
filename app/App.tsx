f3a3e550a24501a8259ca38e9835dc72805a993d
Chrahuldeveloper <chrahulofficial@gmail.com>
2026-02-03 15:40:34 +0530

    dn

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,14 @@
+import { Text, View } from 'react-native';
 import './global.css';
-import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView} from 'react-native-safe-area-context';
 
 export default function App() {
+
   return (
-    <>
-      <StatusBar style="auto" />
-    </>
+    <SafeAreaView>
+    <View>
+      <Text>This view uses custom insets.</Text>
+    </View>
+    </SafeAreaView>
   );
 }
